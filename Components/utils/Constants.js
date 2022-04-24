b2178971c1331d5ae94fa4796dae1cde3beb0db8
Vishakh <vishakhg98@gmail.com>
2022-04-24 18:07:41 +0530

    bride groom venue date and timer constants

--- Components/utils/Constants.js
+++ Components/utils/Constants.js
@@ -1,27 +1,27 @@
 export const VENUE_DATE = 1654453800000;
+export const GROOM_NAME = "Sreekumar";
+export const BRIDE_NAME = "Amrutha";
+export const IMAGE_PATH = "/assets/images/"
 
 export function CountDownTimer(countDownDate) {
-  const timerInterval = setInterval(() => {
-    // Get today's date and time
-    const now = new Date().getTime();
+  // Get today's date and time
+  const now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    const distance = countDownDate - now;
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // If the count down is finished
-    if (distance < 0) {
-      clearInterval(timerInterval);
-      return { distance: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
-    }
+  // If the count down is finished
+  if (distance <= 0) {
+    return { distance: 0, days: 0, hours: 0, minutes: 0, seconds: 0 };
+  }
 
-    return { distance, days, hours, minutes, seconds };
-  }, 1000);
+  return { distance, days, hours, minutes, seconds };
 }
