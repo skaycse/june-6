4833ca086a1fc680a392a076fdc8ef9aea35e432
Vishakh <vishakhg98@gmail.com>
2022-05-05 14:55:23 +0530

    Removed footer

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,18 @@
 import Head from "next/head";
 import Home from "../Components/Home/Home";
-import Footer from "../Components/Footer/Footer";
 
 export default function Index() {
   return (
-    <div className={""}>
+    <>
       <Head>
         <title>Wedding Invitation</title>
-        <meta name="description" content="Wedding invitation website" />
+        <meta name="description" content="Wedding invitation home" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={""}>
+      <main>
         <Home />
-        <Footer />
       </main>
-    </div>
+    </>
   );
 }
