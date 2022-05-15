4dfb83a3a0423bbb1ee6a6075097ad647e999f58
Vishakh <vishakhg98@gmail.com>
2022-05-16 02:53:56 +0530

    Venue time fix

--- Components/utils/Constants.js
+++ Components/utils/Constants.js
@@ -1,4 +1,4 @@
-export const VENUE_DATE = "2022-06-06T00:00:00-06:30";
+export const VENUE_DATE = "2022-06-06T09:00:00-06:30";
 export const VENUE_DURATION = 2 * 60 * 60 * 1000; // 2hours
 export const GROOM_NAME = "Sreekumar";
 export const BRIDE_NAME = "Amrutha";
