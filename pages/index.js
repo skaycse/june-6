2ed4d6eabbb218a0c09f5a6a53eea4625a551b93
Vishakh <vishakhg98@gmail.com>
2022-06-05 09:55:37 +0530

    Timer fix

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Index() {
         <link rel="icon" href="/favicon.ico" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
         <link
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;600;700&family=Inter:wght@400;700&display=swap"
           rel="stylesheet"
